d2eefede5a5a376c385cfad0f980c00599782f3d
fungusta <peterjosephfung@gmail.com>
2025-01-02 03:16:35 +0800

    fix bug

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,21 @@
 import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === 'production';
+const REPO_NAME = 'portfolio-page'
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true, // Disable default image optimization
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'github.com',
+        pathname: '/**',
+      },
+    ],
   },
   assetPrefix: isProd ? '/portfolio-page/' : '',
-  basePath: isProd ? '/portfolio-page' : '',
+  basePath: isProd ? `${REPO_NAME}` : '',
   output: 'export'
 };
 
